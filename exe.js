0c6323fda03e0c6b6bd54d70740ce4fd763f3b98
yassminn7 <yasmin.leticia.pereira@escola.pr.gov.br>
2025-05-16 14:06:41 +0000

    finalizado

---
+++ exe.js
@@ -0,0 +1,48 @@
+// 1. Criar variável `nome` e exibir a frase
+let nome = "Seu Nome";
+console.log("Olá, meu nome é " + nome + ".");
+
+// 2. Criar variáveis `idade` e `cidade` e exibir a frase
+let idade = 25;
+let cidade = "Curitiba";
+console.log("Tenho " + idade + " anos e moro em " + cidade + ".");
+
+// 3. Criar variáveis com comidas favoritas e exibir a frase
+let comida1 = "pizza";
+let comida2 = "sushi";
+let comida3 = "lasanha";
+console.log("Minhas comidas favoritas são: " + comida1 + ", " + comida2 + " e " + comida3 + ".");
+
+// 4. Criar variável `preco` e exibir a frase
+let preco = 50;
+console.log("O produto custa R$ " + preco + ".");
+
+// 5. Criar variáveis `produto` e `quantidade` e exibir a frase
+let produto = "maçã";
+let quantidade = 3;
+console.log("Comprei " + quantidade + " unidades de " + produto + ".");
+
+// 6. Criar variável booleana `temDesconto` e exibir se há desconto
+let temDesconto = true;
+console.log("Tem desconto disponível? " + temDesconto);
+
+// 7. Criar variáveis `anoAtual` e `anoNascimento` e calcular idade
+let anoAtual = 2025;
+let anoNascimento = 1995;
+let idadeCalculada = anoAtual - anoNascimento;
+console.log("Você tem " + idadeCalculada + " anos.");
+
+// 8. Criar variáveis `curso` e `instituicao` e exibir a frase
+let curso = "Engenharia";
+let instituicao = "Universidade XYZ";
+console.log("Estou cursando " + curso + " no " + instituicao + ".");
+
+// 9. Criar variáveis com cores favoritas e exibir a frase
+let cor1 = "azul";
+let cor2 = "verde";
+let cor3 = "vermelho";
+console.log("Minhas cores preferidas são " + cor1 + ", " + cor2 + " e " + cor3 + ".");
+
+// 10. Criar variável `mensagem` e exibir seu valor
+let mensagem = "Seja bem-vindo ao mundo da programação!";
+console.log(mensagem);
